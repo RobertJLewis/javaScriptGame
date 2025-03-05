4c1b5e6bf3681b82c680e8228fdbeec5ccfec5cf
Robert lewis <you@example.com>
2025-03-05 21:28:00 +0000

    adding in comments to let varibles in JS.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
-let politicianTile;
-let reporterTiles = [];
-let score = 0;
-let gameOver = false;
-let timeLeft = 60;
-let politicianClicks = 0;
+// Variables to keep track of the game state
+let politicianTile; // The tile where the politician is
+let reporterTiles = []; // An array to track all active reporter tiles
+let score = 0;       // Player's score
+let gameOver = false; // Is the game over?
+let timeLeft = 60;   // Total game time in seconds
+let politicianClicks = 0; // Track how many times the politician has been clicked
